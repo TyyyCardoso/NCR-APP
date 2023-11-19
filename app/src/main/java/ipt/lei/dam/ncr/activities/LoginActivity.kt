f182b61fe732dfd7e188e462fc425ed24c015ccf
Tiago Ourique Cardoso <86069519+TyyyCardoso@users.noreply.github.com>
2023-11-19 16:58:13 +0000

    Project Structure + Splash Screen commit

---
+++ app/src/main/java/ipt/lei/dam/ncr/activities/LoginActivity.kt
@@ -0,0 +1,25 @@
+package ipt.lei.dam.ncr.activities
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import ipt.lei.dam.ncr.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        val usernameEditText = findViewById<EditText>(R.id.username)
+        val passwordEditText = findViewById<EditText>(R.id.password)
+        val loginButton = findViewById<Button>(R.id.login_button)
+
+        loginButton.setOnClickListener {
+            val username = usernameEditText.text.toString()
+            val password = passwordEditText.text.toString()
+            // Implement your login logic here
+        }
+    }
+}
---
+++ app/src/main/java/ipt/lei/dam/ncr/activities/SplashActivity.kt
@@ -0,0 +1,51 @@
+package ipt.lei.dam.ncr.activities
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationSet
+import android.view.animation.AnimationUtils
+import androidx.appcompat.app.AppCompatActivity
+import ipt.lei.dam.ncr.MainActivity
+import ipt.lei.dam.ncr.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.Theme_NCRAPP)
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        val logoView = findViewById<View>(R.id.logo_view)
+        val loadingImageView = findViewById<View>(R.id.loading_image) // Your loading image view
+
+        // Load the logo animation
+        val logoAnimation = AnimationUtils.loadAnimation(this, R.anim.logo_animation)
+
+        // Load the rotation animation
+        val rotateLoadingAnimation = AnimationUtils.loadAnimation(this, R.anim.rotate_loading)
+
+        // Start the logo animation
+        logoView.startAnimation(logoAnimation)
+
+        // Listener for logo animation
+        logoAnimation.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(animation: Animation?) {}
+
+            override fun onAnimationEnd(animation: Animation?) {
+                // Start rotation animation after logo animation ends
+                loadingImageView.startAnimation(rotateLoadingAnimation)
+
+                // Delay for 2 seconds before starting MainActivity
+                Handler(Looper.getMainLooper()).postDelayed({
+                    startActivity(Intent(this@SplashActivity, LoginActivity::class.java))
+                    finish()
+                }, 4000)  // 2000 milliseconds delay
+            }
+
+            override fun onAnimationRepeat(animation: Animation?) {}
+        })
+    }
+}
