0209a712e72ef46ff6feacc8924f967106235aff
Tiago Ourique Cardoso <86069519+TyyyCardoso@users.noreply.github.com>
2023-12-29 00:52:55 +0000

    Created Drawer navigation menu base

--- app/src/main/java/ipt/lei/dam/ncrapp/activities/MainActivity.kt
+++ app/src/main/java/ipt/lei/dam/ncrapp/activities/MainActivity.kt
@@ -14,31 +14,41 @@ import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.widget.Toolbar
 import androidx.core.content.ContextCompat
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.navigation.NavigationView
 import ipt.lei.dam.ncrapp.R
 import ipt.lei.dam.ncrapp.activities.authentication.LoginActivity
 import ipt.lei.dam.ncrapp.databinding.ActivityMainBinding
 
-class MainActivity : BaseActivity() {
+class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedListener {
+
     private lateinit var binding: ActivityMainBinding
+    private lateinit var drawerLayout: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+
         //Referencia ao BottomNavigationView
         val navView: BottomNavigationView = binding.navView
+        drawerLayout = binding.drawerLayout
 
+        val navigationDrawerView = findViewById<NavigationView>(R.id.nav_side_view)
+
+        navigationDrawerView.setNavigationItemSelectedListener(this)
 
 
 
@@ -57,6 +67,8 @@ class MainActivity : BaseActivity() {
 
         val sharedPref = getSharedPreferences("UserInfo", AppCompatActivity.MODE_PRIVATE)
         var clientType = sharedPref.getString("clientType", "student");
+
+
 
         navView.setOnItemSelectedListener { item ->
             when (item.itemId) {
@@ -93,6 +105,7 @@ class MainActivity : BaseActivity() {
 
         }
 
+
         val toolbar: Toolbar = findViewById(R.id.toolbar_custom)
         val toolbarBackButton: ImageView = findViewById(R.id.back_button)
         val toolbarLoginContainer: LinearLayout = findViewById(R.id.entrar_container)
@@ -109,6 +122,8 @@ class MainActivity : BaseActivity() {
             toolbarLoginContainerText.text = "Sair"
             toolbarLoginImage.setImageResource(R.drawable.baseline_logout_24) // Use o nome do resource drawable
         }
+
+
 
 
         toolbarLoginContainer.setOnClickListener {
@@ -129,6 +144,31 @@ class MainActivity : BaseActivity() {
         }
 
 
+        val toggle = ActionBarDrawerToggle(this,drawerLayout,toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
 
+        navigationDrawerView.setCheckedItem(R.id.navigation_events)
     }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.navigation_profile -> {
+                findNavController(R.id.nav_host_fragment_activity_main).navigate(R.id.navigation_profile)
+                true
+            }
+            R.id.navigation_sabias -> {
+                findNavController(R.id.nav_host_fragment_activity_main).navigate(R.id.navigation_sabias)
+                true
+            }
+            R.id.navigation_events -> {
+                findNavController(R.id.nav_host_fragment_activity_main).navigate(R.id.navigation_events)
+                true
+            }
+            else -> true
+        }
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
 }
